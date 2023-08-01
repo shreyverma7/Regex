9a3ab998bf77def78339acb5ff0978cb566c05d5
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-01 17:06:57 +0530

    [Shrey] Add : Regex Folder creation

---
+++ Regex/Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\"REGEX - Regular Expression - Used for Validation purposes\r\n\r\n\r\nLogin Pages or Registration Pages\r\n\r\nBoth Front end (UI) and Backend (.Net, Java etc)\r\n\r\nHow can we implement:-\r\n\r\n^ - Starting of the String\r\n$ - End of the String\r\n[a-z] - Small Character’s with unlimited Length\r\n[a-z] {6} - Small Character’s with constant Length\r\n[a-z]{3,} - Small Character’s with minimum constant Length and Max unlimited Length\r\n[a-zA-z] - Mixing of Character’s\r\n[0-9] - number format\r\n[a-zA-Z0-9] - All characters\r\n\r\n\r\nManish - “^[A-Z]{1}[a-z]{5}$” - FirstName and LastName\r\nPincode - “^[0-9]{6}$”\r\nPassword - “^[a-zA-Z0-9@_-]$”\"\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t");
+    }
+}
