d085fc7d6b24b241790e97ae9ebb7a5abfde3477
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-02 11:41:00 +0530

    [Shrey] Add : Validate the Email first part in REGEX

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -4,17 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Regex Functionality");
-        ValidatePincode validatePincode = new ValidatePincode();
-        validatePincode.UC1_Validate("601201");
-        validatePincode.UC2_Validate("H601201");
-        validatePincode.UC3_Validate("601201H");
-        validatePincode.UC4_Validate("601 201");
-        validatePincode.UC4_Validate("601201");
-
-
-
-
-
+        int num = 2;
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("Regex Functionality");
+                ValidatePincode validatePincode = new ValidatePincode();
+                validatePincode.UC1_Validate("601201");
+                validatePincode.UC2_Validate("H601201");
+                validatePincode.UC3_Validate("601201H");
+                validatePincode.UC4_Validate("601 201");
+                validatePincode.UC4_Validate("601201");
+                break;
+            case 2:
+                ValidateEmail email = new ValidateEmail();
+                email.UC1_Validate("abc"); 
+                break;
+        }
+      
     }
 }
---
+++ Regex/ValidateEmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblems
+{
+    public class ValidateEmail
+    {
+        //abc.xyz@bridgelabz.co.in 
+        string uc1_Regex = "^[a-z]{0,}$";
+        public void UC1_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1_Regex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation UnSuccessful");
+        }
+    }
+}
