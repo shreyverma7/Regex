7629d791bfa8fcabba9b39e80e7f2cd9cb764726
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-04 13:09:47 +0530

    [Shrey] Add : User registration tested with Unit tested

--- Regex/UserRegistrationProblem/UserValidation.cs
+++ Regex/UserRegistrationProblem/UserValidation.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace RegexProblems.UserRegistrationProblem
 {
-    internal class UserValidation
+    public class UserValidation
     {
         string uc1_FirstName = "^[A-Z]{1}[a-z]{2,}$";
         string uc2_LastName = "^[A-Z]{1}[a-z]{2,}$";
@@ -15,41 +15,75 @@ namespace RegexProblems.UserRegistrationProblem
         string uc6_PasswordCap = "^(?=.*[A-Z]).{8,}$"; 
         string uc7_PasswordNum = "^(?=.*\\d)(?=.*[A-Z]).{8,}$"; 
         string uc7_PasswordCapNumSym = "^(?=.*\\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$"; 
-        string uc8_PasswordCapNumSym = "^(?=.*\\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$"; 
+        string uc8_PasswordCapNumSym = "^(?=.*\\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$";
+        string actualRegex = "^[a-z]+[._+-]{0,1}[a-z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2,}){0,1}$";
+        string uc4_Mobile = "^[0-9]{2}[ ]{1}[0-9]{10}$";
 
-        string uc4_Mobile = "^[0-9]{2}[ ]{1}[0-9]{10}$ || ^[0-9]{10}$";
+        public bool UC1_FirstName(string input)
+        {
+            if( Regex.IsMatch(input, uc1_FirstName)){
+                Console.WriteLine("Validation Successful");
+                return true;
 
-        public void UC1_FirstName(string input)
-        {
-            bool result = Regex.IsMatch(input, uc1_FirstName);
-            if (result)
-                Console.WriteLine("Validation Successful");
+            }
             else
+            {
                 Console.WriteLine("Validation UnSuccessful");
+                return false;
+            }
         }
-        public void UC2_LastName(string input)
+        public bool UC2_LastName(string input)
         {
-            bool result = Regex.IsMatch(input, uc2_LastName);
-            if (result)
+            if (Regex.IsMatch(input, uc2_LastName)){
                 Console.WriteLine("Validation Successful");
+                return true;
+
+            }
             else
+            {
                 Console.WriteLine("Validation UnSuccessful");
+                return false;
+            }
         }
-        public void UC4_Mobile(string input)
+        public bool UC4_Mobile(string input)
         {
-            bool result = Regex.IsMatch(input, uc4_Mobile);
-            if (result)
+            if (Regex.IsMatch(input, uc4_Mobile)){
                 Console.WriteLine("Validation Successful");
+                return true;
+
+            }
             else
+            {
                 Console.WriteLine("Validation UnSuccessful");
+                return false;
+            }
         }
-        public void UC5_Password(string input)
+        public bool UC5_Password(string input)
         {
-            bool result = Regex.IsMatch(input, uc8_PasswordCapNumSym);
-            if (result)
+            if (Regex.IsMatch(input, uc8_PasswordCapNumSym)){
                 Console.WriteLine("Validation Successful");
+                return true;
+
+            }
             else
+            {
                 Console.WriteLine("Validation UnSuccessful");
+                return false;
+            }
+        }
+        public bool UC6_Email(string input)
+        {
+            if (Regex.IsMatch(input, actualRegex))
+            {
+                Console.WriteLine("Validation Successful");
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine("Validation UnSuccessful");
+                return false;
+            }
         }
     }
 }
